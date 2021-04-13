cd38b451979e387386754a0d27e77de3ec34c4aa
Crypticwave <abhinavdatta38@gmail.com>
2021-04-13 10:50:08 +0530

    Better Help

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,37 @@
 const fs = require('fs');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+//const folders = fs.readdirSync('./commands', { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
 const { embed } = require('../templates/completeEmbed');
 
 module.exports = {
 
   name: 'help',
   description: 'Display the help menu',
+  category: 'Misc',
   execute(message, args){
     let desc = "";
+    let cmds = [];
+    let categories = [];
     for (const file of commandFiles) {
     	const command = require(`./${file}`);
-      desc += `**\`${command.name}\`** - ${command.description}\n`;
-
+      cmds.push({
+                  category: command.category,
+                  name: command.name,
+                  desc: command.description 
+                });
+      if(!categories.includes(command.category)){
+        categories.push(command.category);
+      }
     }
+    categories.forEach(c => {
+      desc += `**${c}**\n`;
+      cmds.forEach(cmd => {
+        if(cmd.category == c){
+          desc += `\`${cmd.name}\` - ${cmd.desc} \n`;
+        }
+      });
+      desc += '\n';
+    });
     message.channel.send({ embed: embed('Help Menu', desc, message.author) });
   }
 
--- commands/test.js
+++ commands/test.js
@@ -4,6 +4,7 @@ module.exports = {
   
   name: 'test',
   description: 'Testing the bot',
+  category: 'Misc',
   execute(message, args){
     message.channel.send({ embed: embed('Testing', '**Test Successful**', message.author) });
   }
